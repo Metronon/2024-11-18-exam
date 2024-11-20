310a7c9dcd1ad1b401824a55529be7a5d78f1942
Metronon <bumpercar07@gmail.com>
2024-11-20 15:42:00 +0900

    5단계 구현

--- 2024-11-18-exam/src/Main.java
+++ 2024-11-18-exam/src/Main.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
+        List<Wise> wises = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        int id = 1;
+        int id = 1; // id 라는 변수를 선언해 명언목록의 순서 설정
         while (true) {
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
@@ -13,17 +17,14 @@ public class Main {
                 sc.close();
                 break;
             } else if (command.equals("등록")) {
-                Wise.addWise(sc, id);
+                Wise.addWise(sc, id, wises);
                 id += 1;
             } else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for(int i = 1; i < id; i++) {
-                    System.out.print(Wise.getWise(-i));
-                    System.out.print(" / ");
-                    System.out.print(Wise.getWise(-i));
-                    System.out.print(" / ");
-                    System.out.println(Wise.getWise(-i));
+                Collections.reverse(wises);
+                for (Wise wise : wises) {
+                    System.out.println(wise.getId() + " / " + wise.getAuthor() + " / " + wise.getSaying());
                 }
             }
         }
@@ -31,19 +32,36 @@ public class Main {
 }
 
 class Wise {
-    private static ArrayList<String> wises = new ArrayList<>();
+    private int id;
+    private String author;
+    private String saying;
 
-    public static void addWise(Scanner sc, int id) {
+    public Wise(int id, String author, String saying) {
+        this.id = id;
+        this.author = author;
+        this.saying = saying;
+    }
+
+    public static void addWise(Scanner sc, int id, List<Wise> wises) {
         System.out.print("명언 : ");
-        wises.add(sc.nextLine().trim());
+        String saying = sc.nextLine().trim();
 
         System.out.print("작가 : ");
-        wises.add(sc.nextLine().trim());
+        String author = sc.nextLine().trim();
 
-        wises.add(String.valueOf(id));
+        wises.add(new Wise(id, author, saying));
         System.out.println(id + "번 명언이 등록되었습니다.");
     }
-    public static String[] getWise(int i) {
-        return wises[i];
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSaying() {
+        return saying;
     }
 }
